245afd05c7763a9a49c6152634888af482db1111
Hagarde <43098624+Hagarde@users.noreply.github.com>
2021-04-06 17:06:49 +0200

    vzvz

--- src/Controller/SIRController.php
+++ src/Controller/SIRController.php
@@ -213,6 +213,7 @@ class SIRController extends AbstractController
             $influence23 =strval($etatavant->getTest22());
             $influence24 =strval($etatavant->getTest21());
             $influence34 =strval($etatavant->getTest22());
+            
             /*
             $command = escapeshellcmd('pip3 install numpy');
             shell_exec($command);
@@ -220,17 +221,23 @@ class SIRController extends AbstractController
             shell_exec($command);
             $command = escapeshellcmd('pip3 install matplotlib');
             shell_exec($command);
+            */
+
             $stringcommand = 'python3 python_script/application_env.py'.' '. $s1 .' '. $s2 .' '. $s3 .' '. $s4 .' '. $u1 .' '. $u2 .' '. $u3 .' '. $u4 .' '. $p1 .' ' . $p2 .' '.$p3. ' ' .$p4.' ' .$ru1. ' '.$ru2. ' '. $ru3 . ' ' . $ru4 . ' ' .$rp1. ' ' . $rp2 . ' '. $rp3 . ' '. $rp4 . ' ' . $R0 . ' ' . $pi . ' '. $mu .' ' . $test11 . ' ' . $test12 . ' '. $test21 . ' ' . $test22 .' '. $influence12 . ' ' . $influence13 . ' '. $influence14 . ' '. $influence23 . ' ' . $influence24 . ' ' . $influence34 ;
             $command = escapeshellcmd($stringcommand .'2>&1');
+            dump($stringcommand );
+
+
+            $strcmd ='python3 python_script/application_env.py 95 95 95 95 5 5 5 5 0 0 0 0 0 0 0 0 0 0 0 0 10 1 0.1 0.25 0.25 0.25 0.25 0 0 0 0 0 0';
+            $command = escapeshellcmd($strcmd);
             $output = shell_exec($command);
             dump($output);
-            */
-            
-            $strcmd ='pyton3 python_script/test.py';
+            /*
+            $strcmd ='python3 python_script/test.py';
             $command= escapeshellcmd($strcmd);
             $output = shell_exec($command);
             dump($output);
-
+            */
             $manager->persist($resultexp);
             $manager->flush();
             return $this->redirectToRoute('exp_form',[]);
